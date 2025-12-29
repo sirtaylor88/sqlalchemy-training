0b98532252995befb220f5aa94c7bab034f947d3
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2025-12-29 14:23:46 +0100

    ✨ First lesson: Create an users table with 2 users.

--- src/lesson_1.py
+++ src/lesson_1.py
@@ -26,5 +26,29 @@ engine = create_engine(
 session_maker = sessionmaker(engine)
 
 with session_maker() as session:
-    session.execute(text("SELECT 1"))
+    session.execute(
+        text("""
+            CREATE TABLE users
+            (
+                telegram_id   BIGINT PRIMARY KEY,
+                full_name     VARCHAR(255) NOT NULL,
+                username      VARCHAR(255),
+                language_code VARCHAR(255) NOT NULL,
+                created_at    TIMESTAMP DEFAULT NOW(),
+                referrer_id   BIGINT,
+                FOREIGN KEY (referrer_id)
+                    REFERENCES users (telegram_id)
+                    ON DELETE SET NULL
+            );
+
+            INSERT INTO users
+                (telegram_id, full_name, username, language_code, created_at)
+            VALUES (1, 'Peter Pan', 'peter', 'en', '2025-12-29');
+
+            INSERT INTO users
+                (telegram_id, full_name, username, language_code, created_at, referrer_id)
+            VALUES (2, 'Adam Evan', 'adam', 'en', '2025-12-29', 1);
+        """)
+    )
+
     session.commit()
